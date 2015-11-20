81bd88f0d914b2de811e31533ab6f364c6e915ba
medwards1771 <meredith.edwards1771@gmail.com>
2015-11-20 06:07:06 -0500

    done

---
+++ app/lib/programmer.js
@@ -0,0 +1,20 @@
+class Programmer {
+  constructor(name, language="Ruby"){
+    this.name = name;
+    this.language = language;
+  }
+
+  evangelize(){
+    var string = `${this.name}: ${this.language.toUpperCase()} IS THE BEST LANGUAGE EVER`;
+    return string;
+  }
+}
+
+export var bestLanguages = ["Ruby", "Elixir"];
+
+export function addJavaScript(bestLanguages){
+  return bestLanguages + ", JavaScript";
+}
+
+
+export default Programmer;
